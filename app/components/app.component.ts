4a1035d7b960ee3b7d5c14800a06cf7e0951b174
knipferrc <knipferrc@gmail.com>
2018-05-04 15:31:12 -0400

    revert

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-component',
-  template: `<h1>{{text}}</h1>`
+  template: `
+    <h1>{{text}}</h1>
+  `
 })
 export class AppComponent {
   public text = 'Angular-Parcel boilerplate'
