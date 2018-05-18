4539c183c9c8918a0b37051e7fe9eb3e65afd70d
knipferrc <knipferrc@gmail.com>
2018-05-18 07:52:37 -0400

    cleanup

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,18 +2,19 @@ import { AppModule } from './app.module'
 import { enableProdMode } from '@angular/core'
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
+if (module['hot']) {
+  module['hot'].accept(() => {
+    window.location.reload()
+  })
+  module['hot'].dispose(() => {
+    window.location.reload()
+  })
+}
+
 if (process.env.NODE_ENV === 'production') {
   enableProdMode()
 }
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
-  .then(() => {
-    if (module['hot']) {
-      module['hot'].accept()
-      module['hot'].dispose(() => {
-        window.location.reload()
-      })
-    }
-  })
   .catch(err => console.error(err))
