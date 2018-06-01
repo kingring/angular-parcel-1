7273408f4e0493df1dbd34295814d1dbea6f5a3b
knipferrc <knipferrc@gmail.com>
2018-06-01 07:47:11 -0400

    use template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core'
+const fs = require('fs')
 
 @Component({
   selector: 'app-home-component',
-  template: '<h1>Home</h1>'
+  template: `${fs.readFileSync(__dirname + '/home.component.html')}`,
+  styles: [`${fs.readFileSync(__dirname + '/home.component.css')}`]
 })
 export class HomeComponent {}
